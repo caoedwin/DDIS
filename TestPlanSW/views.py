dde88d2e3d796304d39ef5b4e3d9ba2c5eaaa7f8
曹泽 <10669375+Cao_Ze@user.noreply.gitee.com>
2025-01-23 10:38:12 +0800

    20250123am

--- TestPlanSW/views.py
+++ TestPlanSW/views.py
@@ -9,7 +9,8 @@ import datetime,json,simplejson
 from django.db.models import Max,Min,Sum,Count,Q
 from django.views.decorators.csrf import csrf_exempt
 
-phase_list = ["B(FVT)", "C(SIT)", "SIT2", 'GSKU', "Wave","Wave2","Wave3","Wave4","Wave5","OOC","OOC2","OOC3","OOC4","OOC5","OOC6","FFRT","FFRT2","FFRT3","FFRT4","FFRT5","FFRT6","Others", 'Downgrade','SIT_U9','U9_FFRT','U9_FFRT2','U9_FFRT3','GSKU_FFRT','GSKU_FFRT2','GSKU_FFRT3','DG_FFRT','DG_FFRT2','DG_FFRT3']
+# phase list 需要与TestPlanSW额edit search里面的phaseName一样
+phase_list = ["B(FVT)", 'FVT Regression1', 'FVT Regression2', 'SIT Regerssion1', 'SIT Regerssion2', 'SIT Regerssion3', 'SIT Regerssion4', 'SIT Regerssion5', "C(SIT)", "SIT2", 'GSKU', "Wave","Wave2","Wave3","Wave4","Wave5","OOC","OOC2","OOC3","OOC4","OOC5","OOC6","FFRT","FFRT2","FFRT3","FFRT4","FFRT5","FFRT6","Others", 'Downgrade','SIT_U9','U9_FFRT','U9_FFRT2','U9_FFRT3','GSKU_FFRT','GSKU_FFRT2','GSKU_FFRT3','DG_FFRT','DG_FFRT2','DG_FFRT3']
 
 # Create your views here.
 def TestPlanSW_summary(request):
@@ -247,7 +248,7 @@ def TestPlanSW_summary(request):
                         HC = 0 if not TestProjectSW.objects.filter(**check_dic_Pro).first().Full_Function_Duration else round(CodeATO / 6 / TestProjectSW.objects.filter(
                                 **check_dic_Pro).first().Full_Function_Duration, 2)
                         HCO = round(HC * 6 / 7, 2)
-                        print(i["name"],HCO)
+                        # print(i["name"],HCO)
                         # mock_data.append(
                         #     {"Project": i["name"], "Phase": i["value"], "CaseLibrarytime": CodeBS, "ProjectBasetime": CodeFS,
                         #      "Timew/Comfig": CodeTCM,
@@ -301,7 +302,7 @@ def TestPlanSW_summary(request):
                         HC_R = 0 if not TestProjectSW.objects.filter(**check_dic_Pro).first().Full_Function_Duration else round(CodeATO_R / 6 / TestProjectSW.objects.filter(
                                 **check_dic_Pro).first().Full_Function_Duration, 2)
                         HCO_R = round(HC_R * 6 / 7, 2)
-                        print(i["name"], HCO)
+                        # print(i["name"], HCO)
                     mock_data.append(
                         {"Project": i["name"], "Phase": i["value"], "CaseLibrarytime": CodeBS + CodeBS_R,
                          "ProjectBasetime": CodeFS + CodeFS_R,
@@ -328,16 +329,18 @@ def TestPlanSW_summary(request):
             # print(Phaselist_top)
 
             # print(Projectlist_top)
-            Phasesortorder = {'B(FVT)': 0, 'C(SIT)': 1, 'SIT2': 2, 'GSKU': 3, 'FFRT': 4, 'FFRT2': 5, 'FFRT3': 6,
-                              'FFRT4': 7,
-                              'FFRT5': 8, 'FFRT6': 9, 'Wave': 10, 'Wave2': 11, 'Wave3': 12, 'Wave4': 13,
-                              'Wave5': 14, 'OOC': 15, 'OOC2': 16, 'OOC3': 17, 'OOC4': 18, 'OOC5': 19, 'OOC6': 20,
-                              'Others': 21,
-                              'Downgrade': 22, 'SIT_U9': 23, 'U9_FFRT': 24, 'U9_FFRT2': 25, 'U9_FFRT3': 26,
-                              'GSKU_FFRT': 27, 'GSKU_FFRT2': 28, 'GSKU_FFRT3': 29,
-                              'DG_FFRT': 30, 'DG_FFRT2': 31, 'DG_FFRT3': 32,
-                              }
-            Phaselist_top.sort(key=lambda x: Phasesortorder[x])
+            # Phasesortorder = {'B(FVT)': 0, 'C(SIT)': 1, 'SIT2': 2, 'GSKU': 3, 'FFRT': 4, 'FFRT2': 5, 'FFRT3': 6,
+            #                   'FFRT4': 7,
+            #                   'FFRT5': 8, 'FFRT6': 9, 'Wave': 10, 'Wave2': 11, 'Wave3': 12, 'Wave4': 13,
+            #                   'Wave5': 14, 'OOC': 15, 'OOC2': 16, 'OOC3': 17, 'OOC4': 18, 'OOC5': 19, 'OOC6': 20,
+            #                   'Others': 21,
+            #                   'Downgrade': 22, 'SIT_U9': 23, 'U9_FFRT': 24, 'U9_FFRT2': 25, 'U9_FFRT3': 26,
+            #                   'GSKU_FFRT': 27, 'GSKU_FFRT2': 28, 'GSKU_FFRT3': 29,
+            #                   'DG_FFRT': 30, 'DG_FFRT2': 31, 'DG_FFRT3': 32,
+            #                   }
+            Phaselist_top.sort(key=lambda x: phase_list.index(x))
+            # print(Phaselist_top)
+            # Phaselist_top.sort(key=lambda x: Phasesortorder[x])
             # print(Phaselist_top)
             for i in Projectlist_top:
                 ProjecttotalATO = 0.00
@@ -1612,17 +1615,18 @@ def TestPlanSW_summary(request):
                 # print(Projectlist_top)
                 Projectlist_top = list(set(Projectlist_top))
                 Phaselist_top = list(set(Phaselist_top))
-                Phasesortorder = {'B(FVT)': 0, 'C(SIT)': 1, 'SIT2': 2, 'GSKU': 3, 'FFRT': 4, 'FFRT2': 5, 'FFRT3': 6,
-                                  'FFRT4': 7,
-                                  'FFRT5': 8, 'FFRT6': 9, 'Wave': 10, 'Wave2': 11, 'Wave3': 12, 'Wave4': 13,
-                                  'Wave5': 14, 'OOC': 15, 'OOC2': 16, 'OOC3': 17, 'OOC4': 18, 'OOC5': 19, 'OOC6': 20,
-                                  'Others': 21,
-                                  'Downgrade': 22, 'SIT_U9': 23, 'U9_FFRT': 24, 'U9_FFRT2': 25, 'U9_FFRT3': 26,
-                                  'GSKU_FFRT': 27, 'GSKU_FFRT2': 28, 'GSKU_FFRT3': 29,
-                                  'DG_FFRT': 30, 'DG_FFRT2': 31, 'DG_FFRT3': 32,
-                                  }
+                # Phasesortorder = {'B(FVT)': 0, 'C(SIT)': 1, 'SIT2': 2, 'GSKU': 3, 'FFRT': 4, 'FFRT2': 5, 'FFRT3': 6,
+                #                   'FFRT4': 7,
+                #                   'FFRT5': 8, 'FFRT6': 9, 'Wave': 10, 'Wave2': 11, 'Wave3': 12, 'Wave4': 13,
+                #                   'Wave5': 14, 'OOC': 15, 'OOC2': 16, 'OOC3': 17, 'OOC4': 18, 'OOC5': 19, 'OOC6': 20,
+                #                   'Others': 21,
+                #                   'Downgrade': 22, 'SIT_U9': 23, 'U9_FFRT': 24, 'U9_FFRT2': 25, 'U9_FFRT3': 26,
+                #                   'GSKU_FFRT': 27, 'GSKU_FFRT2': 28, 'GSKU_FFRT3': 29,
+                #                   'DG_FFRT': 30, 'DG_FFRT2': 31, 'DG_FFRT3': 32,
+                #                   }
+                Phaselist_top.sort(key=lambda x: phase_list.index(x))
                 # print(Projectlist_top)
-                Phaselist_top.sort(key=lambda x: Phasesortorder[x])
+                # Phaselist_top.sort(key=lambda x: Phasesortorder[x])
                 for i in Projectlist_top:
                     ProjecttotalATO = 0.00
                     ProjecttotalRe = 0.00
