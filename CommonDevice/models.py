e79e31170b34a17d36b2cb4b33cf03e3516bb7ed
曹泽 <10669375+Cao_Ze@user.noreply.gitee.com>
2025-05-22 17:02:37 +0800

    20250522am

--- CommonDevice/models.py
+++ CommonDevice/models.py
@@ -2,9 +2,49 @@ from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 from app01.models import UserInfo
 # Create your models here.
+
+class Category(models.Model):
+    """大类模型"""
+    name = models.CharField("大类名称", max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    """细项模型（依赖大类）"""
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
+    name = models.CharField("细项名称", max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class CommonDevice(models.Model):
-    Category = models.CharField("大项", max_length=50)
-    Product_Type = models.CharField("细项", max_length=50)
+    # Category_choice = (
+    #     # ('Select Customer', 'Select Customer'),
+    #     ('', ''),
+    #     ('網絡設備', '網絡設備'),
+    #     ('自動化設備', '自動化設備'),
+    # )
+    # Product_Type_choice = (
+    #     # ('Select Customer', 'Select Customer'),
+    #     ('', ''),
+    #     ('宽带', '宽带'),
+    #     ('伺服器', '伺服器'),
+    #     ('交換機', '交換機'),
+    #     ('自動化設備', '自動化設備'),
+    # )
+    Status_choice = (
+        # ('Select Customer', 'Select Customer'),
+        ('', ''),
+        ('使用中', '使用中'),
+        ('損壞', '損壞'),
+        ('閑置', '閑置'),
+        ('報廢', '報廢'),
+    )
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="大类")
+    Product_Type = models.ForeignKey(SubCategory, on_delete=models.CASCADE, verbose_name="细项")
+    # Category = models.CharField("大项", choices=Category_choice, max_length=50)
+    # Product_Type = models.CharField("细项", choices=Product_Type_choice, max_length=50)
     Name = models.CharField("Name", max_length=50, null=True, blank=True)
     Num = models.CharField("號碼", max_length=50, null=True, blank=True)
     Manufacturer = models.CharField("廠商", max_length=50, null=True, blank=True)
@@ -15,6 +55,7 @@ class CommonDevice(models.Model):
     Department = models.CharField("部別", max_length=50)
     Location = models.CharField("位置", max_length=50, null=True, blank=True)
     Asset_Num = models.CharField("資產編號", max_length=50, null=True, blank=True)
+    Dev_Status = models.CharField("狀態", choices=Status_choice, max_length=50, null=True, blank=True)
     Owner = models.ManyToManyField("app01.UserInfo")
     # Owner = models.CharField("负责人", max_length=50, null=True, blank=True)
     Mail = models.EmailField("郵件地址", null=True, blank=True)
--- CommonDevice/views.py
+++ CommonDevice/views.py
@@ -4,11 +4,12 @@ import datetime, json, simplejson, requests, time
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from app01.models import UserInfo
+from django.db import transaction
 from django.db.models import Max, Min, Sum, Count, Q, F, Value, CharField
 from django.db.models.functions import Substr
 from operator import itemgetter, attrgetter
 from collections import Counter
-from .models import CommonDevice
+from .models import CommonDevice, Category, SubCategory
 from django.db.models.functions import ExtractYear
 
 
@@ -21,21 +22,48 @@ def CommonDevice_edit(request):
     if not Skin:
         Skin = "/static/src/blue.jpg"
     weizhi = "DepartmentManage/公共設備"
-    categoryOptions = [
-        # "網絡設備1", "網絡設備2"
-    ]
-    for i in CommonDevice.objects.all().values('Category').distinct():
-        categoryOptions.append(
-            i['Category']
-        )
 
-    ProductTypeOptions = [
-        # "智慧家庭", "宽带"
-                          ]
-    for i in CommonDevice.objects.all().values('Product_Type').distinct():
-        ProductTypeOptions.append(
-            i['Product_Type']
+    allCategorys = {}
+    for i in Category.objects.all():
+        allCategorys[i.id] = i.name
+
+    allSubCategorys = {}
+    for i in SubCategory.objects.all():
+        print(i)
+        allSubCategorys[i.id] = i.name
+
+    selectItem = [
+        # {
+        #   'id': 1,
+        #   'name': '網絡設備',
+        #   'SubCategorys': [
+        #     { id: 11, 'name': '宽带' },
+        #     { id: 12, 'name': '伺服器' }
+        #   ]
+        # },
+      ]
+    for i in Category.objects.all():
+        SubCategorylist = []
+
+        for j in SubCategory.objects.filter(category__name=i.name):
+            SubCategorylist.append(
+                {"id": j.id, 'name': j.name}
+            )
+        selectItem.append(
+            {
+                'id': i.id,
+                'name': i.name,
+                'SubCategorys': SubCategorylist
+            }
         )
+    # print(selectItem, 'selectItem')
+
+
+
+    StatusOptions = []
+    for i in CommonDevice._meta.get_field('Dev_Status').choices:
+        StatusOptions.append(i[0])
+    # print(StatusOptions, 'StatusOptions')
 
     sectionOwner = [
         # {"value": "姚麗麗", "number": "22314345"}, {"value": "張亞萍", "number": "123456789"},
@@ -47,12 +75,7 @@ def CommonDevice_edit(request):
             }
         )
 
-    # classOptions = ["Sensor test device", "USB Keyboard", "USB Mouse"]
 
-    # DeviceOptions = [
-    #     {"key": "姚丽丽", "label": "2222222"},
-    #     {"key": "张颖", "label": "1111111"},
-    # ]
 
     OwnersOptions = [
         # {"key": "姚丽丽", "label": "2222222"},
@@ -61,7 +84,7 @@ def CommonDevice_edit(request):
     for i in UserInfo.objects.all():
         OwnersOptions.append(
             {
-                "key": i.username, "label": i.account
+                "account": i.account, "username": i.username, "id": i.id
             }
         )
 
@@ -86,17 +109,19 @@ def CommonDevice_edit(request):
     ]
 
     errMsg = ''
-    permission = 1  # 權限  0是有權限 其他數字沒有權限
+    permission = 1  # 權限
+    SSVIP_users = 0
     roles = []
     onlineuser = request.session.get('account')
     if UserInfo.objects.filter(account=onlineuser).first():
         for i in UserInfo.objects.filter(account=onlineuser).first().role.all():
             roles.append(i.name)
     # print(roles)
-    # editPpriority = 100
-    # for i in roles:
-    #     if i == 'admin' or i == 'PublicAreaAdmin':
-    #         permission = 0
+    for i in roles:
+        if i == 'admin' or i == 'DQA_director':
+            SSVIP_users = 1
+        if i == 'admin' or i == 'PublicAreaAdmin':
+            permission = 1
     mock_obj = CommonDevice.objects.all()
     if request.method == "POST":
         if request.POST:
@@ -109,9 +134,128 @@ def CommonDevice_edit(request):
                     ch_dic['Owner'] = user
                 if ch_dic:
                     mock_obj = CommonDevice.objects.filter(**ch_dic)
+            if request.POST.get('action') == 'addData':
+                # ID = request.POST.get('ID')
+                Owners = json.loads(request.POST.get('Owner'))
+                # print(Owners)
+                Mail = ''
+                UserInfos = UserInfo.objects.filter(account__in=Owners)
+                # print(UserInfos)
+                for i in UserInfos:
+                    Mail += '%s/' % i.email.split('@')[0]
+                # print(Mail)
+                Mail = Mail[:-1] + '@Compal.com'
+                # print(Mail)
+                create_dic = {
+                    "Category_id": request.POST.get('Category'),
+                    "Product_Type_id": request.POST.get('Product_Type'),
+                    "Name": request.POST.get('Name') if request.POST.get('Name') else '',
+                    "Num": request.POST.get('Num') if request.POST.get('Num') else '',
+                    "Manufacturer": request.POST.get('Manufacturer') if request.POST.get('Manufacturer') else '',
+                    "Cost": request.POST.get('Cost') if request.POST.get('Cost') else '',
+                    "Account": request.POST.get('Account') if request.POST.get('Account') and request.POST.get(
+                        'Account') != 'null' else None,
+                    "PW": request.POST.get('PW') if request.POST.get('PW') else '',
+                    "Purchasing_Date": request.POST.get('Purchasing_Date') if request.POST.get(
+                        'Purchasing_Date') and request.POST.get('Account') != 'None' and request.POST.get(
+                        'Account') != 'null' else None,
+                    "Department": request.POST.get('Department') if request.POST.get('Department') else '',
+                    # 从有值变成空值的更新,整型，浮点型，日期都是None
+                    "Location": request.POST.get('Location') if request.POST.get('Location') else '',
+                    "Asset_Num": request.POST.get('Asset_Num') if request.POST.get('Asset_Num') else '',
+                    "Dev_Status": request.POST.get('Status') if request.POST.get('Status') else '',
+                    "Mail": Mail,
+                    "Contact_info": request.POST.get('Contact_info') if request.POST.get('Contact_info') else '',
+                    "Comments": request.POST.get('Comments') if request.POST.get('Comments') else '',
+                    "Creator": onlineuser,
+                }
+                # try:
+                with transaction.atomic():
+                    Comdevice = CommonDevice.objects.create(**create_dic)
+                    Comdevice.Owner.add(*UserInfos)
+
+                    Comdevice.Category = Category.objects.get(id=request.POST.get('Category'))
+                    Comdevice.Product_Type = SubCategory.objects.get(id=request.POST.get('Product_Type'))
+                    Comdevice.save()
+                # except Exception as e:
+                #     # alert = '此数据正被其他使用者编辑中...'
+                #     errMsg = alert = str(e)
+                #     print(alert)
+
+                # mock_data
+                check_dic = {}
+                if request.POST.get('searchCategory'):
+                    check_dic['Category'] = request.POST.get('searchCategory')
+                if request.POST.get('searchOwner'):
+                    user = UserInfo.objects.filter(account=request.POST.get('searchOwner')).first()
+                    check_dic['Owner'] = user
+                if check_dic:
+                    mock_obj = CommonDevice.objects.filter(**check_dic)
+            if request.POST.get('action') == 'update':
+                ID = request.POST.get('ID')
+                Owners = json.loads(request.POST.get('Owner'))
+                # print(Owners)
+                Mail = ''
+                UserInfos = UserInfo.objects.filter(account__in=Owners)
+                # print(UserInfos)
+                for i in UserInfos:
+                    Mail += '%s/' % i.email.split('@')[0]
+                # print(Mail)
+                Mail = Mail[:-1] + '@Compal.com'
+                # print(Mail)
+                update_dic = {
+                    "Category_id": request.POST.get('Category'),
+                    "Product_Type_id": request.POST.get('Product_Type'),
+                    "Name": request.POST.get('Name') if request.POST.get('Name') else '',
+                    "Num": request.POST.get('Num') if request.POST.get('Num') else '',
+                    "Manufacturer": request.POST.get('Manufacturer') if request.POST.get('Manufacturer') else '',
+                    "Cost": request.POST.get('Cost') if request.POST.get('Cost') else '',
+                    "Account": request.POST.get('Account') if request.POST.get('Account') and request.POST.get(
+                        'Account') != 'null' else None,
+                    "PW": request.POST.get('PW') if request.POST.get('PW') else '',
+                    "Purchasing_Date": request.POST.get('Purchasing_Date') if request.POST.get(
+                        'Purchasing_Date') and request.POST.get('Account') != 'None' and request.POST.get(
+                        'Account') != 'null' else None,
+                    "Department": request.POST.get('Department') if request.POST.get('Department') else '',
+                    # 从有值变成空值的更新,整型，浮点型，日期都是None
+                    "Location": request.POST.get('Location') if request.POST.get('Location') else '',
+                    "Asset_Num": request.POST.get('Asset_Num') if request.POST.get('Asset_Num') else '',
+                    "Dev_Status": request.POST.get('Status') if request.POST.get('Status') else '',
+                    "Mail": Mail,
+                    "Contact_info": request.POST.get('Contact_info') if request.POST.get('Contact_info') else '',
+                    "Comments": request.POST.get('Comments') if request.POST.get('Comments') else '',
+                }
+                try:
+                    with transaction.atomic():
+                        CommonDevice.objects.filter(id=ID).update(**update_dic)
+
+                        Comdevice = CommonDevice.objects.get(id=ID)
+                        Comdevice.Owner.clear()
+                        Comdevice.Owner.add(*UserInfos)
+
+                        # Comdevice.Category = Category.objects.get(id=request.POST.get('Category'))
+                        # Comdevice.Product_Type = SubCategory.objects.get(id=request.POST.get('Product_Type'))
+                        # Comdevice.save()
+
+                except Exception as e:
+                    # alert = '此数据正被其他使用者编辑中...'
+                    errMsg = alert = str(e)
+                    print(alert)
+
+                # mock_data
+                check_dic = {}
+                if request.POST.get('searchCategory'):
+                    check_dic['Category'] = request.POST.get('searchCategory')
+                if request.POST.get('searchOwner'):
+                    user = UserInfo.objects.filter(account=request.POST.get('searchOwner')).first()
+                    check_dic['Owner'] = user
+                if check_dic:
+                    mock_obj = CommonDevice.objects.filter(**check_dic)
+
         else:
             try:
                 request.body
+
             except:
                 pass
             else:
@@ -133,14 +277,19 @@ def CommonDevice_edit(request):
             Owner_list = []
             Ownerflag = False
             for j in i.Owner.all():
-                Owner_list.append({'Name': j.username, 'Num': j.account})
+                Owner_list.append(
+                    {
+                        "account": j.account, "username": j.username, "id": j.id
+                    }
+                )
                 if not Ownerflag:
                     if j.account == onlineuser:
                         Ownerflag = True
 
             mock_data.append(
                 {
-                    "id": i.id, "Category": i.Category, "Product_Type": i.Product_Type,
+                    "id": i.id, "Category": Category.objects.filter(name=i.Category).first().id,
+                    "Product_Type": SubCategory.objects.filter(name=i.Product_Type).first().id,
                     "Name": i.Name,
                     "Num": i.Num,
                     "Manufacturer": i.Manufacturer,
@@ -150,7 +299,9 @@ def CommonDevice_edit(request):
                     "Purchasing_Date": str(i.Purchasing_Date) if i.Purchasing_Date else '',
                     "Duration": Duration,
                     "Department": i.Department,
+                    "Location": i.Location,
                     "Asset_Num": i.Asset_Num,
+                    "Status": i.Dev_Status,
                     "Owner": Owner_list,
                     "Mail": i.Mail,
                     "Contact_info": i.Contact_info,
@@ -158,21 +309,24 @@ def CommonDevice_edit(request):
                     "Creator": i.Creator,
                     # "Ownerflag": i.Creator == onlineuser, creator才有权限。
                     "Ownerflag": Ownerflag, # 只要是Owner中的一个有权限，Owner改变了权限就会随之变化
+                    'CreatedBy': UserInfo.objects.filter(account=i.Creator).first().username if UserInfo.objects.filter(account=i.Creator).first() else '',
                 }
             )
-
+        print(mock_data)
         data = {
             "errMsg": errMsg,
-            "categoryOptions": categoryOptions,
-            "ProductTypeOptions": ProductTypeOptions,
-            # "classOptions": classOptions,
             "sectionOwner": sectionOwner,
-            # "DeviceOptions": DeviceOptions,
             "OwnersOptions": OwnersOptions,
+            "StatusOptions": StatusOptions,
+            "allSubCategorys": allSubCategorys,
+            "allCategorys": allCategorys,
             "content": mock_data,
             "permission": permission,
+            "SSVIP_users": SSVIP_users,
+            "selectItem": selectItem,
 
         }
+        # print(data)
         return HttpResponse(json.dumps(data), content_type="application/json")
     return render(request, 'CommonDevice/CommonDevice.html', locals())
 
